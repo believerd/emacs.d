579eff0b05ab62c51f3c998104e2ff5597fa4258
believerd <30063871+believerd@users.noreply.github.com>
2020-08-09 22:24:47 +0800

    Initial setups and first commit.

---
+++ init.el
@@ -0,0 +1,21 @@
+;;;------------------------------------------
+;;; Believerd's Emacs Config
+;;;------------------------------------------
+
+;; Use org to config emacs
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
