80c7955d2d37fbc265708c05bf70b41fca881cac
believerd <30063871+believerd@users.noreply.github.com>
2020-09-11 19:59:50 +0800

    use magit!

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "e1d09f1b2afc2fed6feb1d672be5ec6ae61f84e058cb757689edb669be926896" "939ea070fb0141cd035608b2baabc4bd50d8ecc86af8528df9d41f4d83664c6a" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" default))
- '(package-selected-packages '(doom-modeline use-package)))
+ '(package-selected-packages '(magit doom-modeline use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
