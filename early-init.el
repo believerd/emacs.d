a10954987759d97038cc25f5e4141e50488d45cb
believerd <30063871+believerd@users.noreply.github.com>
2021-02-24 23:06:20 +0800

    updated

--- early-init.el
+++ early-init.el
@@ -23,7 +23,5 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-(when (featurep 'ns)
-  (push '(ns-transparent-titlebar . t) default-frame-alist))
 
 ;;; early-init.el ends here
