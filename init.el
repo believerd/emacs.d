15db5c8078d7a2eae9a8dc2d7ff512cdb5b38cf3
believerd <30063871+believerd@users.noreply.github.com>
2021-03-25 23:16:01 +0800

    markdown mode

--- init.el
+++ init.el
@@ -435,3 +435,10 @@
   "q" 'kill-buffer-and-window)
   :custom
   (setq url-automatic-caching t))
+
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
